f305bf7cdcb09dc869bdda947728d98228c5ae7f
heeyun <hy_0127@naver.com>
2024-12-20 12:56:51 +0900

    Add content collections config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,37 +1,32 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		category: z.string().optional(),
-		tags: z.array(z.string()).default(['others']),
-		published: z.boolean().default(false),
-	})
-});
-
 const portfolio = defineCollection({
 	type: 'content',
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		pubDate: z.coerce.date(),
+		pubDate: z.date(),
 		category: z.string(),
 		tech: z.array(z.string()),
+		published: z.boolean()
+	})
+});
+
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		updatedDate: z.date().optional(),
 		heroImage: z.string().optional(),
-		published: z.boolean(),
-		links: z.array(z.object({
-			title: z.string(),
-			url: z.string()
-		})).default([])
+		category: z.string(),
+		tags: z.array(z.string()).optional(),
+		published: z.boolean()
 	})
 });
 
 export const collections = {
-	'blog': blog,
-	'portfolio': portfolio,
+	portfolio,
+	blog
 };
