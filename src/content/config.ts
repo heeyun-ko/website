1ba64d0f3a355c7928ed0f5c4cd68ad530fc0d48
heeyun <hy_0127@naver.com>
2025-03-18 21:01:19 +0900

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
-const portfolio = defineCollection({
+const main = defineCollection({
 	type: 'content',
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.date(),
-		category: z.string(),
-		tech: z.array(z.string()),
+		category: z.literal('main'),
+		tech: z.array(z.string()).optional(),
 		published: z.boolean()
 	})
 });
 
-const blog = defineCollection({
+const postings = defineCollection({
 	type: 'content',
 	schema: z.object({
 		title: z.string(),
@@ -20,13 +20,26 @@ const blog = defineCollection({
 		pubDate: z.date(),
 		updatedDate: z.date().optional(),
 		heroImage: z.string().optional(),
-		category: z.string(),
+		category: z.literal('postings'),
 		tags: z.array(z.string()).optional(),
 		published: z.boolean()
 	})
 });
 
+const cvDownload = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		category: z.literal('CV download'),
+		fileUrl: z.string(),
+		published: z.boolean()
+	})
+});
+
 export const collections = {
-	portfolio,
-	blog
+	main,
+	postings,
+	cvDownload
 };
