e6938dd6823d3d4b8e87edb163e461fbfedeeee2
heeyun <hy_0127@naver.com>
2025-03-27 19:20:08 +0900

    jj

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,4 +36,22 @@ const blog = defineCollection({
 	})
 });
 
-export const collections = { main, postings, blog };
+const posts = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		updatedDate: z.date().optional(),
+		heroImage: z.string().optional(),
+		meta_title: z.string().optional(),
+		meta_description: z.string().optional(),
+		keywords: z.array(z.string()).optional(),
+		og_title: z.string().optional(),
+		og_description: z.string().optional(),
+		og_type: z.string().optional(),
+		twitter_card: z.string().optional(),
+	}),
+});
+
+export const collections = { main, postings, blog, posts };
