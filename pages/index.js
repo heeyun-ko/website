116d23a8e8d7806c8bebcde8089711aafd7cc1f3
heeyun <hy_0127@naver.com>
2025-03-31 17:20:23 +0900

    cursor

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Header from '@/components/Header'
-
-export default function Home() {
-  return (
-    <main className="container mx-auto px-4 py-6">
-      <Header />
-    </main>
-  );
-} 
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Point of View by Eindel';
-export const SITE_DESCRIPTION = 'Transform your Obsidian notes into a beautiful blog';
